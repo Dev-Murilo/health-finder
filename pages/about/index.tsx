3a89b17019c6283de30af4b98ccecf20f5cea0c7
Murilo Silva <silvamurilo689@gmail.com>
2022-12-31 16:09:56 -0300

    feat: adding first pages in project

---
+++ pages/about/index.tsx
@@ -0,0 +1,15 @@
+import { Text } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Heath Finder - About</title>
+      </Head>
+      <main>
+        <Text>Page in construction</Text>
+      </main>
+    </>
+  );
+}
